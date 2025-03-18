7156aa75cf53980abd0e0f1fbaa60bb9a0bac867
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-03-18 18:51:03 +0300

    Update Tarea4.sql

--- Tarea4.sql
+++ Tarea4.sql
@@ -1,6 +1,8 @@
 
-1. SELECT * FROM Customers WHERE ContactName LIKE 'C%';
-2. SELECT * FROM Orders WHERE (Freight BETWEEN 100 AND 200)
+4.3.1.. SELECT * FROM Customers WHERE ContactName LIKE 'C%';
+
+4.3.2. SELECT * FROM Orders WHERE (Freight BETWEEN 100 AND 200)
    AND ShipCountry IN ('USA', 'France'); 
-3. SELECT  DISTINCT EmployeeID FROM EmployeeTerritories WHERE (TerritoryID BETWEEN 06897 AND 31000);
+
+4.3.3. SELECT  DISTINCT EmployeeID FROM EmployeeTerritories WHERE (TerritoryID BETWEEN 06897 AND 31000);
 
