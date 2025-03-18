282b5128d791892f35b827d92510ad61e203bfb2
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-03-18 18:20:56 +0300

    Update Tarea4.sql

--- Tarea4.sql
+++ Tarea4.sql
@@ -1,12 +1,3 @@
-Рефлексия:
-По ошибке отправила решение к задаче 3 не ссылкой на гибхаб, а выложила все в окно.
-Сыылка на решение задания 3:
-https://github.com/SvetlanaBoichenko/SQL/blob/main/Tarea3.sql
-
-По задачам к заданию 3 - все верно.
-
-
-  Решение:
 
 1. SELECT * FROM Customers WHERE ContactName LIKE 'C%';
 2. SELECT * FROM Orders WHERE (Freight BETWEEN 100 AND 200)
