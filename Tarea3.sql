5650cb1086ac5a34d4a2238911a53515e379b12b
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-03-18 18:36:34 +0300

    Update Tarea3.sql

--- Tarea3.sql
+++ Tarea3.sql
@@ -1,8 +1,8 @@
 
-1.	Select ProductName, QuantityPerUnit From Products;
-2.	Select ProductName, UnitPrice From Products WHERE (UnitPrice < 20); 
-3.	SELECT * FROM Orders where (Freight > 11.7) AND (Freight < 98.1); 
-4.	Select * From Employees  where (TitleOfCourtesy = 'Mr.');
-5.	Select * From Suppliers  where (Country = 'Japan');
-6.	Select * From Orders  where (EmployeeID = 2) OR (EmployeeID = 4) OR (EmployeeID = 8); 
-7.	Select OrderID, ProductID From [Order Details] where (UnitPrice > 40) AND (Quantity < 10);
+3.9.1.	Select ProductName, QuantityPerUnit From Products;
+3.9.2.	Select ProductName, UnitPrice From Products WHERE (UnitPrice < 20); 
+3.9.3.	SELECT * FROM Orders where (Freight > 11.7) AND (Freight < 98.1); 
+3.9.4.	Select * From Employees  where (TitleOfCourtesy = 'Mr.');
+3.9.5.	Select * From Suppliers  where (Country = 'Japan');
+3.9.6.	Select * From Orders  where (EmployeeID = 2) OR (EmployeeID = 4) OR (EmployeeID = 8); 
+3.9.7.	Select OrderID, ProductID From [Order Details] where (UnitPrice > 40) AND (Quantity < 10);
