2e9cc3db0f410e1c6b4c5acd26f714d69cf5d89b
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-03-21 14:37:10 +0300

    Update Tarea5.sql

--- Tarea5.sql
+++ Tarea5.sql
@@ -1,3 +1,4 @@
 
 5.4.1. SELECT * FROM Employees ORDER BY BirthDate, Country; 
 5.4.2. SELECT * FROM  Employees  WHERE NOT Region  IS  NULL ORDER BY BirthDate, Country;
+5.4.3. SELECT AVG (UnitPrice), MIN(UnitPrice), MAX(UnitPrice) FROM [Order Details];
