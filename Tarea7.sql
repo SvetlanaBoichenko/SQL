f8a6d1ee3d46685290c0fad3f5019fdd721014c4
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-03-23 16:55:35 +0300

    Update Tarea7.sql

--- Tarea7.sql
+++ Tarea7.sql
@@ -1,1 +1,5 @@
 
+7.3.1. SELECT (Discount*100)/UnitPrice FROM [Order Details];
+
+7.3.2. SELECT * FROM [Order Details] WHERE ProductId IN 
+ (SELECT UnitsInStock FROM Products WHERE UnitsInStock > 40);
