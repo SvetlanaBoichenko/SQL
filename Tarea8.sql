806ebeada2072131e719497027daaea2e5c59732
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-04-01 10:33:10 +0300

    Update Tarea8.sql

--- Tarea8.sql
+++ Tarea8.sql
@@ -7,5 +7,10 @@ WHERE Products.CategoryID = Categories.CategoryID;
 8.3.2 
 SELECT Products.ProductName, [Order Details].UnitPrice
 From Products, [Order Details]
+Where [Order Details].UnitPrice < 20; 
+
+8.3.3
+SELECT Products.ProductName, Categories.CategoryName, [Order Details].UnitPrice
+From Products,Categories, [Order Details] 
 Where [Order Details].UnitPrice < 20;
 
