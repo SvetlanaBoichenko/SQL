172669440a5c047f6af517b74d06c24c924c0d18
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-04-04 01:45:55 +0300

    Update Tarea9.sql

--- Tarea9.sql
+++ Tarea9.sql
@@ -6,3 +6,12 @@ WHERE (t2.Region IS NULL) and
  (t1.CustomerID <> t2.CustomerID) 
 
 9.4.2
+Select * FROM Orders t1 
+WHERE EXISTS 
+(SELECT * FROM  Customers t3 
+WHERE t1.CustomerID= t3.CustomerID  AND t3.Region IS NOT NULL );
+
+9.4.3
+SELECT * FROM Orders t1
+where t1.Freight > ANY 
+(SELECT UnitPrice FROM  Products)
