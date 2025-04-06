ce83074dae804e75789ffafe4e6dcf99e42a1ba9
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-04-06 13:30:33 +0300

    Update Tarea10.sql

--- Tarea10.sql
+++ Tarea10.sql
@@ -3,4 +3,14 @@
 SELECT Products.ProductName, [Order Details].UnitPrice
 FROM Products Join [Order Details]
 ON Products.ProductID = [Order Details].ProductID
-AND [Order Details].UnitPrice <20
+AND [Order Details].UnitPrice <20;
+
+10.4.1
+SELECT Orders.Freight, Customers.CompanyName
+FROM Orders FULL JOIN Customers
+ON Orders.CustomerID = Customers.CustomerID
+ORDER BY Freight;
+Появились две новые записи, те, что пересекаются вывелись по одному разу и добавились те,
+которые не подошли под условие - получили полный набор неповторяющихся записей
+
+10.4.2
