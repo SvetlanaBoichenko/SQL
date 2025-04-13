77a8418ecdb0e3e17edaf8fb25eeb8b3872e5c26
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-04-13 15:54:04 +0300

    Update Tarea11.sql

--- Tarea11.sql
+++ Tarea11.sql
@@ -2,3 +2,11 @@
 SELECT Customers.CompanyName, Orders.CustomerID FROM Customers 
 LEFT JOIN Orders ON Orders.CustomerID is null 
 ORDER BY Customers.CompanyName;
+
+SELECT 'Customer' As Type, ContactName, City, Country FROM Customers 
+WHERE Country='USA' 
+UNION 
+SELECT 'Supplier' As Type, ContactName,City, Country FROM Suppliers 
+WHERE Country='USA' 
+ORDER BY City; 
+
