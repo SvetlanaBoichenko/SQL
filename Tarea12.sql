02577ad3d9ae9af2ba7f66c73aefc4f9eb174271
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-04-18 09:29:02 +0300

    Update Tarea12.sql

--- Tarea12.sql
+++ Tarea12.sql
@@ -1,1 +1,8 @@
+12.3.1
+INSERT INTO Employees (Lastname, FirstName,Title,TitleOfCourtesy,  
+BirthDate, HireDate, Address, City, Region, PostalCode,Country, HomePhone,Extension,  
+Notes, ReportsTo)  
+VALUES ('Boychenko','Svet','Bailadora','Flamemco', '1970-27-08','2025-03-01',   
+  'Ronda Magdalena 14-16', 'Castellon',  
+'Valencia', '12004','España','12345','6789','Baila cada dia', '2');  
 
