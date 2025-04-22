c5741b02817388283184cb544eebb535f4fdf26b
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-04-22 13:09:02 +0300

    Update Tarea13.sql

--- Tarea13.sql
+++ Tarea13.sql
@@ -1,5 +1,13 @@
-13.3.1
-Update [Order Details]
-Set Discount  = '0.20'
-where Quantity > 50;
+13.3.1  
+Update [Order Details]  
+Set Discount  = '0.20'  
+where Quantity > 50;  
+  
+13.3.2  
+Update Contacts 
+Set City  = 'Piter', Country = 'Russia'  
+where City = 'Berlin' and Country = 'Germany';  
+
+
+13.3.3
 
