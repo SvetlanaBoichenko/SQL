d96ca1c765fefffae2f58d08c612ef697510e9de
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-04-30 19:32:40 +0300

    Update Tarea16.sql

--- Tarea16.sql
+++ Tarea16.sql
@@ -1,1 +1,9 @@
 
+Создание индекса для поля RegionID для таблицы Region:
+CREATE UNIQUE INDEX IX_RegionTerritory ON Region (RegionDescription)
+
+
+Создание индекса для поля TerritoryDescription для таблицы Territory:
+
+CREATE UNIQUE INDEX Terr_Description ON Territory (TerritoryDescription)
+
