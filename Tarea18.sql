4a150bbe331aa8cb2ddb2d4a8ee089ab42dcf4e0
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-05-05 15:58:46 +0300

    Update Tarea18.sql

--- Tarea18.sql
+++ Tarea18.sql
@@ -1,9 +1,19 @@
 
-1.
+1.Получить информацию о всех гномах, которые входят в какой-либо отряд, 
+вместе с информацией об их отрядах.
+       
 SELECT Dwarves.name, Dwarves.age, Dwarves.profession, 
        Squads.name as squadsName, Squads.mission as squadsMision
 FROM Dwarves INNER JOIN Squads
 ON  Dwarves.squad_id = Squads.squad_id and Dwarves.squad_id IS NOT NULL;
 
 2. 
+Найти всех гномов с профессией "miner", которые не состоят ни в одном отряде.
+       
+select name from Dwarves
+where Dwarves.squad_id IS NULL and Dwarves.name = 'miner'
+
+3.
+Получить все задачи с наивысшим приоритетом, которые находятся в статусе "pending".
+
 
