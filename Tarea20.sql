1df0a94a0d363d3896975d9ca977822978d7b514
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-05-11 19:38:08 +0300

    Update Tarea20.sql

--- Tarea20.sql
+++ Tarea20.sql
@@ -1,47 +1,20 @@
 
-1. Найдите все отряды, у которых нет лидера. 
+1. 
 Select name as  SqNoLeader
 from Squaqs
 where leader_id IS NULL
 
-2. Получите список всех гномов старше 150 лет, у которых профессия "Warrior".
+2.
 Select name, age
 From Dwarves
 Where age : 150 and profession = '"Warrior';
 
-3. Найдите гномов, у которых есть хотя бы один предмет типа "weapon".
+3. 
   Select name, age
  from From Dwarves
  where Dwarves.dwarf_id = Items.owner_id  and Items.type = 'weapon';
-
-  Field        | Type         | Description                               |
-|--------------|--------------|-------------------------------------------|
-| dwarf_id     | INT          | Уникальный идентификатор гнома            |
-| name         | VARCHAR(100) | Имя гнома                                 |
-| age          | INT          | Возраст гнома                             |
-| profession   | VARCHAR(100) | Профессия гнома                           |
-| squad_id     | INT          | Идентификатор отряда                      
-                                (NULL, если не в отряде)                  |  
-                                (ссылка на dwarf_id из таблицы Dwarves)   | 
-Таблица Squads
-| Field        | Type         | Description                               |
-|--------------|--------------|-------------------------------------------|
-| squad_id     | INT          | Уникальный идентификатор отряда           |
-| name         | VARCHAR(100) | Название отряда                           |
-| leader_id    | INT          | Идентификатор лидера отряда 
-                                (ссылка на dwarf_id из таблицы Dwarves)   |
     
-Tasks
-| Field        | Type         | Description                               |
-|--------------|--------------|-------------------------------------------|
-| task_id      | INT          | Уникальный идентификатор задачи           |
-| description  | TEXT         | Описание задачи                           |
-| assigned_to  | INT          | Идентификатор гнома, ответственного за задачу 
-                                (NULL, если не назначена)                 |
-| status       | VARCHAR(50)  | Статус задачи                             
-                        (например, 'pending', 'in_progress', 'completed') |
-    
-4.  Получите количество задач для каждого гнома, сгруппировав их по статусу.
+4. 
 Select  Dr.name, count (T.task_id)  as TaskCount
 
    FROM 
@@ -53,12 +26,22 @@ Select  Dr.name, count (T.task_id)  as TaskCount
    GROUP BY 
        Dr.dwarf_id, T.status; 
 
-5. Найдите все задачи, которые были назначены гномам из отряда с именем "Guardians".
-
-Select description as GuardiansTask
-From Task, Dwarves
+5. 
+Select D.description as GuardiansTask
+From Task T, Dwarves D
 Where 
-Task.assigned_to = Dwarves.dwarf_id 
-And Dwarves.squad_id = ´Guardians´;
+T.assigned_to = D.dwarf_id 
+And D.squad_id = ´Guardians´;
 
 6.
+Select D.name,  R.relashionShip, D2.name  
+from Dwarves D, Relationships R, Dwarves D2
+IN D2.name = (Select name from Dwarves,Relationships
+              where Dwarves.dwarf_id = Relationships.related_to )
+  
+and R.dwarf_id = D.dwarf_id  
+and (Rrelationship = 'Родитель' or Rrelationship = 'Супруг);
+
+
+
+  
