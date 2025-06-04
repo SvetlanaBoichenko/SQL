eaf56f18c304e12fc2c84a497a44680661d16f1d
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-06-05 02:05:39 +0300

    Update Part2_Tarea12.sql

--- Part2_Tarea12.sql
+++ Part2_Tarea12.sql
@@ -25,7 +25,8 @@ LEFT JOIN SQUAD_BATTLES sqb ON  sqb.squad_id = ms.squad_id
 LEFT JOIN SQUAD_MEMBERS sqm ON sqm.squad_id = ms.squad_id 
 LEFT JOIN DWARV_EQUIPMENT dqe ON dqe.equipment_id = sqe.equipment_id AND sqe.squad_id = ms.squad_id
 LEFT JOIN SQUAD_TRAINING sqt ON sqt.squad_id = ms.squad_id
-), SQ_LEADERS AS 
+), 
+SQ_LEADERS AS 
 (   
   Select  
    ms1.squad_id
@@ -57,8 +58,8 @@ C_SKILLS_PROGRESS AS
 SELECT
 sm.squard_id, 
       
-SUM ( COALESCE(ds_after.level,0)-COALESCE(ds_before.level,0) )
-AS total_skill_improvement
+AVG ( COALESCE(ds_after.level,0)-COALESCE(ds_before.level,0) )
+AS avg_combat_skill_improvement
 
 FROM squad_members sm
 JOIN dwarves d ON sm.dwarf_id = d.dwarf_id
@@ -70,13 +71,6 @@ JOIN squad_operation so ON so.squad_id = sm.squad_id
 WHERE ds_before.date < so.start_date AND ds_after.date > so.end_date
 GROUP BY sm.squad_id
 )
-
-
-
-
-
-
-
       
 Select (
       SA.squad_id,
@@ -95,22 +89,24 @@ Select (
       SA.total_training_sessions.
       SA.avg_training_effectiveness,
       SF.training_battle_correlation,
-      SF.
-
-
-ROUND(AVG (cs.avg_skill_level),2) AS average_combat_skill,      
+      SCP.avg_combat_skill_improvement.      
       
-ROUND( (SA.victory_percentage)*0.35 +
-+ (SA.retention_rate)*0.15  
-+(SA.avg_training_effectiveness  *0.15)
-+(avg_combat_skill_impro * 0.20)
-
-
-AS overall_success_score,
+      ROUND( (SA.victory_percentage)*0.35 +
+      + (SA.retention_rate)*0.15  
+      +(SA.avg_training_effectiveness  *0.15)
+      +(avg_combat_skill_improvement * 0.20)
+      AS overall_effectiveness_score,
 )
 
-FROM  SQUAD_ACTIVITY
-LEFT JOIN SQ_LEADERS SL
-LEFT JOIN SQ_EFFECTIVE SF
+JSON_OBJECT(
+'member_ids', (SELECT JSON_ARRAYAGG (smem.dwarf_id)
+FROM squad_members smem 
+WHERE smem.squad_id = SA.expedition_id ),
 
-LEFT JOIN COMBAT_skills cs ON SA.squad_id = cs.squad_id
+
+      
+FROM  SQUAD_ACTIVITY SA
+LEFT JOIN SQ_LEADERS SL ON  SL.squad_id = SA.squad_id
+LEFT JOIN SQ_EFFECTIVE SF ON SF.squad_id = SA.squad_id
+LEFT JOIN C_SKILLS_PROGRESS SCP ON  SCP.squad_id = SA.squad_id
+GROUP BY overall_effectiveness_score DESC
