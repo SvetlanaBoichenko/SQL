2fda57dbd45cba767bb374cd1e519b9229bc6582
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-06-15 22:33:52 +0300

    Update P2_Tarea16.sql

--- P2_Tarea16.sql
+++ P2_Tarea16.sql
@@ -1,6 +1,24 @@
 
+Историю всех атак существ и их исходов
+
+With creature_statiscic AS (
+ Select cr.creature_id, 
+        cr.type AS creature_type,                             -- Goblin  
+        SUM (cr.threat_level) AS threat_level,                --Or Sum
+        MAX(cr.last_sighting_date) AS last_sighting_date,     -- Последняя дата наблюдения
+        SUM (cr.estimate_population) AS estimated_numbers
+        ct.distance_to_fortress AS territory_proximity
+
+  From Creatures cr
+  Left join Creature_Territories ct ON  cr.creature_id = ct.creature_id AND  ct.danger_leve = cr.threat_level  
+  Where cr.active = true
+  Order By cr.type, cr.creature_id
 
 
+  
+
+        SUM (cr.thread_level) AS current_threat_level
+  
 total_recorded_attacks": 183,
   "unique_attackers": 42,
   "overall_defense_success_rate": 76.50,
