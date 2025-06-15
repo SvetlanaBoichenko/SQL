4ffec77ad261d44f487a53139a18b1e291c2b2e7
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-06-15 12:40:14 +0300

    Create P2_Tarea16.sql

---
+++ P2_Tarea16.sql
@@ -0,0 +1,25 @@
+total_recorded_attacks": 183,
+  "unique_attackers": 42,
+  "overall_defense_success_rate": 76.50,
+  "security_analysis": {
+    "threat_assessment": {
+      "current_threat_level": "Moderate",
+      "active_threats": [
+        {
+          "creature_type": "Goblin",
+          "threat_level": 3,
+          "last_sighting_date": "0205-08-12",
+          "territory_proximity": 1.2,
+          "estimated_numbers": 35,
+          "creature_ids": [124, 126, 128, 132, 136]
+        },
+        {
+          "creature_type": "Forgotten Beast",
+          "threat_level": 5,
+          "last_sighting_date": "0205-07-28",
+          "territory_proximity": 3.5,
+          "estimated_numbers": 1,
+          "creature_ids": [158]
+        }
+      ]
+    },
